504696a72504da20937e33de1ea9ffc0e1d72d6c
Andrew Barton <abarton37@gatech.com>
2024-08-13 10:31:23 -0400

    initial commit

---
+++ sample_script.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+
+def main():
+    array = np.array([1, 2, 3, 4, 5])
+    print("Array:", array)
+    print("Sum of array:", np.sum(array))
+
+if __name__ == "__main__":
+    main()
